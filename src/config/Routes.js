f29ef913620aa1186e3940c26544ef3dfb886d7a
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-28 10:35:02 +0700

    mod routes

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,10 +1,14 @@
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Cart from "../pages/Cart";
+import Account from "../pages/Account";
+import Category from "../pages/Category";
 
 const routes = [
   {
     path: "/",
     component: Home,
+    
   },
   {
     path: "/login",
@@ -13,6 +17,19 @@ const routes = [
   {
     path: "/home",
     component: Home,
+  },
+  {
+    path: "/category",
+    component: Category,
+  },
+  {
+    path: "/cart",
+    component: Cart,
+  },
+  
+  {
+    path: "/account",
+    component: Account,
   },
 ];
 
