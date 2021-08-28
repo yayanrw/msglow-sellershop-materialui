422183bcee69cc3762a237737e7a6090b9853ae9
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-28 09:27:47 +0700

    add routes

---
+++ src/config/Routes.js
@@ -0,0 +1,19 @@
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/login",
+    component: Login,
+  },
+  {
+    path: "/home",
+    component: Home,
+  },
+];
+
+export default routes;
