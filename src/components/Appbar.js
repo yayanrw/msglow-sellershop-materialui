ce43c5e2ccea5d5b7072b166e7fc6c2b5b41d10a
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-30 12:05:54 +0700

    custom appbar component

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+} from "@material-ui/core";
 import msglowsellershop from "../msglowsellershop.png";
 import PropTypes from "prop-types";
+import IconButton from "@material-ui/core/IconButton";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,6 +17,9 @@ const useStyles = makeStyles((theme) => ({
   },
   menuButton: {
     marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
   },
   logo: {
     margin: "auto",
@@ -21,9 +31,6 @@ const useStyles = makeStyles((theme) => ({
 
 function ElevationScroll(props) {
   const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
@@ -37,26 +44,46 @@ function ElevationScroll(props) {
 
 ElevationScroll.propTypes = {
   children: PropTypes.element.isRequired,
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window: PropTypes.func,
 };
 
 function Appbar(props) {
+  const { isLogo, isBack, isProminent, title, color } = props;
   const classes = useStyles();
+
   return (
     <div className={classes.root}>
       <ElevationScroll {...props}>
-        <AppBar position="fixed" color="inherit">
-          <Toolbar variant="dense">
-            <img
-              className={classes.logo}
-              src={msglowsellershop}
-              alt="msglow-sellershop"
-            />
-          </Toolbar>
+        <AppBar position="fixed" color={color}>
+          {isLogo === true ? (
+            <Toolbar>
+              <img
+                className={classes.logo}
+                src={msglowsellershop}
+                alt="msglow-sellershop"
+              />
+            </Toolbar>
+          ) : isProminent === true ? (
+            <Toolbar>
+              <Typography variant="h6" className={classes.title}>
+                Prominent
+              </Typography>
+            </Toolbar>
+          ) : isBack === true ? (
+            <Toolbar>
+              <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="back"
+              >
+                <ArrowBackIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                {title}
+              </Typography>
+            </Toolbar>
+          ) : null}
         </AppBar>
       </ElevationScroll>
     </div>
