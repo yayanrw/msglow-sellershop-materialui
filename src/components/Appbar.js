21d49d30ceef7632757144d7fa551ebac7cdb642
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-30 11:15:59 +0700

    mod logo and elevate when scrolled

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
 import msglowsellershop from "../msglowsellershop.png";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,23 +12,53 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   logo: {
-    height: "25px",
+    margin: "auto",
+    textAlign: "center",
+    maxWidth: "50%",
+    maxHeight: "70%",
   },
 }));
 
-function Appbar() {
+function ElevationScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+    target: window ? window() : undefined,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+ElevationScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+function Appbar(props) {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <img
-            className={classes.logo}
-            src={msglowsellershop}
-            alt="msglow-sellershop"
-          />
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll {...props}>
+        <AppBar position="fixed" color="inherit">
+          <Toolbar variant="dense">
+            <img
+              className={classes.logo}
+              src={msglowsellershop}
+              alt="msglow-sellershop"
+            />
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
     </div>
   );
 }
