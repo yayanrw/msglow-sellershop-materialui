b596d69aeee176c3db32e5ef320257d9dd9f3cfe
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-30 12:37:40 +0700

    mod category

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Appbar from "../components/Appbar";
 
 function Category() {
-  return <div>Category</div>;
+  return (
+    <div>
+      <Appbar isLogo={false} isBack={false} title="Category" color="primary" />
+    </div>
+  );
 }
 
 export default Category;
