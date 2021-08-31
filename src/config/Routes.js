44bd752d9a6fe66242abaca5ba241f1f423375c4
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-31 13:45:30 +0700

    mod routes split login

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,19 +1,9 @@
-import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Cart from "../pages/Cart";
 import Account from "../pages/Account";
 import Category from "../pages/Category";
 
 const routes = [
-  {
-    path: "/",
-    exact: true,
-    component: Login,
-  },
-  {
-    path: "/login",
-    component: Login,
-  },
   {
     path: "/home",
     component: Home,
