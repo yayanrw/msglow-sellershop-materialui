95642bc8c21b6b83230ebe0f30613f54dffad273
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-31 13:45:56 +0700

    custom layouting page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import { ThemeProvider } from "@material-ui/styles";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import React from "react";
 import MyTheme from "./config/MyTheme";
 import routes from "./config/Routes";
 import Layout from "./components/Layout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <ThemeProvider theme={MyTheme}>
       <Router>
-        <Layout>
-          <Switch>
-            {routes.map((route, i) => (
-              <RouteWithSubRoutes key={i} {...route} />
-            ))}
-          </Switch>
-        </Layout>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          <Redirect path="/" exact to="/login" />
+          <Layout>
+            <Switch>
+              {routes.map((route, i) => (
+                <RouteWithSubRoutes key={i} {...route} />
+              ))}
+            </Switch>
+          </Layout>
+        </Switch>
       </Router>
     </ThemeProvider>
   );
