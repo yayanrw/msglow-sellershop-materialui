c2d7d45e583d1bd326ac5a6c329fa343ff4f5b7c
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-31 16:02:51 +0700

    add searchbar and banner

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,10 +1,27 @@
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 import Appbar from "../components/Appbar";
+import Banner from "../components/Banner";
+import SearchBar from "../components/SearchBar";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    paddingBottom: "72px",
+  },
+  prominent: {
+    backgroundColor: theme.palette.primary.main,
+  },
+}));
 
 function Category() {
+  const classes = useStyle();
   return (
-    <div>
+    <div className={classes.root}>
       <Appbar isLogo={false} isBack={false} title="Category" color="primary" />
+      <div className={classes.prominent}>
+        <SearchBar />
+        <Banner elevation={3} />
+      </div>
     </div>
   );
 }
