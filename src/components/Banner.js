d1c456e843899dcf3382b3dda88f75d6c3bb0fb1
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-08-31 09:29:18 +0700

    add skeleton

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,11 +8,12 @@ import {
   makeStyles,
   Typography,
 } from "@material-ui/core";
-import React from "react";
+import { Skeleton } from "@material-ui/lab";
+import React, { useEffect, useState } from "react";
 
 const useStyle = makeStyles((theme) => ({
   root: {
-    paddingTop: "8px",
+    paddingTop: "16px",
     paddingBottom: "16px",
     display: "flex",
     flexDirection: "row",
@@ -28,6 +29,11 @@ const useStyle = makeStyles((theme) => ({
     padding: 0,
     backgroundColor: theme.palette.primary.light,
   },
+  skeletonList: {
+    width: "300px",
+    margin: 0,
+    padding: 0,
+  },
   title: {
     textDecoration: "bold",
   },
@@ -35,28 +41,44 @@ const useStyle = makeStyles((theme) => ({
 
 function Banner() {
   const classes = useStyle();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <List className={classes.root}>
+    <List className={classes.root + " hidescroll"}>
       {Array(5)
         .fill()
         .map((item, index) => {
           return (
             <ListItem key={index} className={classes.listItem}>
-              <Card className={classes.cardList} elevation={0}>
-                <CardContent>
-                  <Typography
-                    variant="h6"
-                    className={classes.title}
-                    color="textSecondary"
-                    gutterBottom
-                  >
-                    Great offers just started now
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Button size="small">Get now</Button>
-                </CardActions>
-              </Card>
+              {loading ? (
+                <Card className={classes.skeletonList} elevation={0}>
+                  <Skeleton variant="rect" height={150} animation="wave" />
+                </Card>
+              ) : (
+                <Card className={classes.cardList} elevation={0}>
+                  <CardContent>
+                    <Typography
+                      variant="h6"
+                      className={classes.title}
+                      color="textSecondary"
+                      gutterBottom
+                    >
+                      Great offers just started now
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Get now</Button>
+                  </CardActions>
+                </Card>
+              )}
             </ListItem>
           );
         })}
